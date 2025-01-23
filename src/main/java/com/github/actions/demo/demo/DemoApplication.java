93ffc0d760a7c683d9c5e1d13c7dd80a76135903
kdigumar <50736473+kdigumar@users.noreply.github.com>
2025-01-24 01:44:16 +0530

    Update DemoApplication.java

--- src/main/java/com/github/actions/demo/demo/DemoApplication.java
+++ src/main/java/com/github/actions/demo/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("jon");
+		System.out.println("jonmilton");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
